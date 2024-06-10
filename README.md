c3d9b3676fdbc003ef7a71507578009a7d63f8bc
fucktradeyoung <2402301712@qq.com>
2024-06-11 01:40:25 +0800

    1
    111

--- README.md
+++
@@ -1,2 +0,0 @@
-# AI_interior_decoration_design_assistant
-the project of the ai system design
--- test_llm_api.py
+++
@@ -1,4 +0,0 @@
-from llm_api.llm_api_single import call_with_stream
-
-if __name__ == '__main__':
-    call_with_stream("Who are you")
